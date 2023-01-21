18230c613acf529d9c554d348f6cd9e7a1d59574
otienochris <otienochris98@gmail.com>
2023-01-21 08:07:36 +0300

    adding ability to view group members in home section

---
+++ src/finitestateautomata/FiniteStateAutomata.java
@@ -0,0 +1,17 @@
+package finitestateautomata;
+
+/**
+ *
+ * @author
+ */
+public class FiniteStateAutomata {
+
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new LandingPage().setVisible(true);
+            }
+        });
+    }
+    
+}
